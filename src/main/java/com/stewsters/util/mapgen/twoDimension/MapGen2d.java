af312aa595e5f5d475d425ba0c9be3066b289725
Adrian.Moore <stewsters@gmail.com>
2014-02-19 16:33:34 -0600

    Flood fill

--- src/main/java/com/stewsters/util/mapgen/twoDimension/MapGen2d.java
+++ src/main/java/com/stewsters/util/mapgen/twoDimension/MapGen2d.java
@@ -5,12 +5,20 @@ import com.stewsters.util.mapgen.twoDimension.brush.Brush2d;
 import com.stewsters.util.mapgen.twoDimension.predicate.CellPredicate2d;
 import com.stewsters.util.math.Point2i;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 
-
+/**
+ *  http://www.roguebasin.com/index.php?title=Designing_Flexible,_Reusable_Algorithms
+ */
 public class MapGen2d {
 
-    // http://www.roguebasin.com/index.php?title=Designing_Flexible,_Reusable_Algorithms
+    /**
+     * Fills an area with a border
+     * @param map
+     * @param fill
+     * @param wall
+     */
     public static void fillWithBorder(GeneratedMap2d map, CellType fill, CellType wall) {
 
         for (int x = 0; x < map.getWidthInTiles(); x++) {
@@ -38,40 +46,46 @@ public class MapGen2d {
         }
     }
 
+
+    /**
+     * Flood fills on things that fit the predicate
+     * @param map
+     * @param start
+     * @param predicate
+     * @param brush2d
+     */
     public static void floodFill(GeneratedMap2d map, Point2i start, CellPredicate2d predicate, Brush2d brush2d) {
-        {
-            LinkedList<Point2i> todo = new LinkedList<Point2i>();
-            LinkedList<Point2i> match = new LinkedList<Point2i>();
 
-            todo.push(start);
+        LinkedList<Point2i> todo = new LinkedList<Point2i>();
+        LinkedList<Point2i> match = new LinkedList<Point2i>();
+        HashSet<Point2i> done = new HashSet<Point2i>();
 
-//            const Map::Cell temp = Map::tempCell();
-//            const Size mapsize = imap->size();
+        todo.push(start);
 
-            Point2i p;
-            while (todo.size() > 0) {
-                p = todo.pop();
+        Point2i p;
+        while (todo.size() > 0) {
+            p = todo.pop();
 
-                if (predicate.belongs(map, p.x, p.y)) {
+            if (done.contains(p) && predicate.belongs(map, p.x, p.y)) {
 
-                    match.add(p);
+                match.add(p);
 
-                    //todo: done list?
-                    if (p.x > 0)
-                        todo.push(new Point2i(p.x - 1, p.y));
-                    if (p.x < map.getWidthInTiles() - 1)
-                        todo.push(new Point2i(p.x + 1, p.y));
-                    if (p.y > 0)
-                        todo.push(new Point2i(p.x, p.y - 1));
-                    if (p.y < map.getHeightInTiles() - 1)
-                        todo.push(new Point2i(p.x, p.y + 1));
-                }
+                //todo: done list?
+                if (p.x > 0)
+                    todo.push(new Point2i(p.x - 1, p.y));
+                if (p.x < map.getWidthInTiles() - 1)
+                    todo.push(new Point2i(p.x + 1, p.y));
+                if (p.y > 0)
+                    todo.push(new Point2i(p.x, p.y - 1));
+                if (p.y < map.getHeightInTiles() - 1)
+                    todo.push(new Point2i(p.x, p.y + 1));
             }
-// Goes over the whole map replacing a cell satisfying the predicate with the brush contents.
+            done.add(p);
+        }
 
-            for (Point2i point2i : match) {
-                brush2d.draw(map, point2i.x, point2i.y);
-            }
+        // Goes over the whole map replacing a cell satisfying the predicate with the brush contents.
+        for (Point2i point2i : match) {
+            brush2d.draw(map, point2i.x, point2i.y);
         }
 
     }
