3555a1aff6a64cc33e614d5b54eb3a4c79b10e6a
Adrian.Moore <stewsters@gmail.com>
2014-02-20 16:31:41 -0600

    Moving some math functions

--- src/main/java/com/stewsters/util/math/MathUtils.java
+++ src/main/java/com/stewsters/util/math/MathUtils.java
@@ -65,6 +65,27 @@ public class MathUtils {
         return source[id];
     }
 
+    public static int min(int a, int b) {
+        if (a < b) return a;
+        return b;
+    }
+
+    public static int min(int a, int b, int c) {
+        if (min(a, b) < c) return min(a, b);
+        return c;
+    }
+
+    public static int max(int a, int b) {
+        if (a > b) return a;
+        return b;
+    }
+
+    public static int pow(int a, int b) {
+        if (b > 1) return a * pow(a, b - 1);
+        else return a;
+    }
+
+
     public static int limit(int number, int low, int high) {
         return Math.max(low, Math.min(high, number));
     }
