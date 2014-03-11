5bcd72664903a6da742f23d7af3bb50c507aee46
Adrian.Moore <stewsters@gmail.com>
2014-03-11 13:02:41 -0500

    Adding a version without the not

---
+++ src/main/java/com/stewsters/util/mapgen/twoDimension/predicate/CellNearCell2d.java
@@ -0,0 +1,28 @@
+package com.stewsters.util.mapgen.twoDimension.predicate;
+
+import com.stewsters.util.mapgen.CellType;
+import com.stewsters.util.mapgen.twoDimension.GeneratedMap2d;
+
+public class CellNearCell2d implements CellPredicate2d {
+
+    CellType cellType;
+
+    public CellNearCell2d(CellType cellType) {
+        this.cellType = cellType;
+    }
+
+    @Override
+    public boolean belongs(GeneratedMap2d generatedMap2d, int x, int y) {
+        for (int ix = -1; ix <= 1; ix++) {
+            for (int iy = -1; iy <= 1; iy++) {
+                if (ix == 0 && iy == 0)
+                    continue;
+                if (generatedMap2d.getCellTypeAt(x + ix, y + iy) == cellType)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+}
