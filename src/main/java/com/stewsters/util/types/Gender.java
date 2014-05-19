5ab3c1fed22f28cbe3d18048cfaa31640a6b81ce
Adrian.Moore <stewsters@gmail.com>
2014-05-19 16:30:41 -0500

    Apparently this doesn't compile in windows

--- src/main/java/com/stewsters/util/types/Gender.java
+++ src/main/java/com/stewsters/util/types/Gender.java
@@ -3,15 +3,9 @@ package com.stewsters.util.types;
 import com.stewsters.util.math.MatUtils;
 
 public enum Gender {
-    MALE('♂'),
-    FEMALE('♀'),
-    NEUTER('⚲');
-
-    public char glyph;
-
-    Gender(char glyph) {
-        this.glyph = glyph;
-    }
+    MALE(),
+    FEMALE(),
+    NEUTER();
 
     public static Gender random() {
         return MatUtils.getBoolean() ? Gender.MALE : Gender.FEMALE;
